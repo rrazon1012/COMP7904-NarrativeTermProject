34a75d55032f785fa524cad904677d3b08919788
zzhang129@my.bcit.ca <zzhang129@my.bcit.ca>
2021-02-27 20:57:40 -0800

    Partial game manager functions set

--- Assets/_Modules/GameMechanics/GameManager.cs
+++ Assets/_Modules/GameMechanics/GameManager.cs
@@ -1,18 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager GM;
+
+    private void Awake()
     {
-        
+        if (GM == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            GM = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if (!SceneManager.GetActiveScene().name.Equals("Main Menu"))
+        {
+            if (GM != this)
+                GM = this;
+
+            EventSystem.current.onPlayerRangeEnter += OnPlayerRangeEnter;
+            EventSystem.current.onPlayerRangeExit += OnPlayerRangeExit;
+            EventSystem.current.onObjectRangeEnter += OnObjectRangeEnter;
+            EventSystem.current.onObjectRangeExit += OnObjectRangeExit;
+            EventSystem.current.onObjectInteract += OnObjectInteract;
+            EventSystem.current.onObjectFinish += OnObjectFinish;
+            EventSystem.current.onPlayerDeath += OnPlayerDeath;
+        }
+    }
+
+    private void OnPlayerRangeEnter()
+    {
+        //Player entered
+    }
+
+    private void OnPlayerRangeExit()
+    {
+        //Player exit
+    }
+
+    private void OnObjectRangeEnter()
+    {
+        //Object entered
+    }
+
+    private void OnObjectRangeExit()
+    {
+        //Object exit
+    }
+
+    private void OnObjectInteract()
+    {
+        //Object interacted
+    }
+
+    private void OnObjectFinish()
+    {
+        //Object Finished
+    }
+
+    private void OnPlayerDeath()
+    {
+        //I die
     }
 }
