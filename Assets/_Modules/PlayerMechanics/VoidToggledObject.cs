d20befddd2e0fed159fd9358a88a0fa49f62326e
Kurtis Lawson <kurtlawson17@gmail.com>
2021-02-28 15:08:51 -0800

    feat(Models): Added textures for mug, several photos, championship medal

--- Assets/_Modules/PlayerMechanics/VoidToggledObject.cs
+++ Assets/_Modules/PlayerMechanics/VoidToggledObject.cs
@@ -17,12 +17,12 @@ public class VoidToggledObject : MonoBehaviour
     [SerializeField] protected bool isRevealed = true;
     [SerializeField] protected List<GameObject> objectList;
 
-    protected Collider collider;
+    protected Collider voidCollider;
 
     // Start is called before the first frame update
     void Start() {
 
-        collider = this.GetComponent<Collider>();
+        voidCollider = this.GetComponent<Collider>();
         objectList = new List<GameObject>();
 
         foreach (Transform child in this.transform) {
@@ -35,7 +35,7 @@ public class VoidToggledObject : MonoBehaviour
         if (revealedInWorld == LightOrDark.Light) {
 
             // A Light-Object is revealed the majority of the time, so we check the exception: the case where we should hide it.
-            if (VoidSphere.Instance.Active && VoidSphere.Instance.ObjectFullyWithinCollision(collider, (collider as SphereCollider).radius)) {
+            if (VoidSphere.Instance.Active && VoidSphere.Instance.ObjectFullyWithinCollision(voidCollider, (voidCollider as SphereCollider).radius)) {
                 
                 // Only iterate and hide each object if they have not already been hidden.
                 if (isRevealed) {
@@ -61,7 +61,7 @@ public class VoidToggledObject : MonoBehaviour
         } else if (revealedInWorld == LightOrDark.Dark) {
 
             // A Dark-Object is hidden the majority of the time, so we check the exception: the case where the void is active and it may be revealed.
-            if (VoidSphere.Instance.Active && VoidSphere.Instance.ObjectJustOnEdgeCollision(collider)) {
+            if (VoidSphere.Instance.Active && VoidSphere.Instance.ObjectJustOnEdgeCollision(voidCollider)) {
                 
                 
                 if (!isRevealed) {
