4e33ca907977ff3848d3fc18ee7350fda8cc97b3
rrazon1012 <rammel1012@gmail.com>
2021-02-27 18:30:28 -0800

    inspect and pickup

---
+++ Assets/_Modules/Interactions/PickupableObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupableObject : InteractableObject
+{
+    [SerializeField] GameObject item_holder;
+    protected bool picked_up = false;
+    void FixedUpdate()
+    {
+        UpdateInteraction();
+        if (picked_up) {
+            transform.position = item_holder.transform.position;
+        }
+    }
+
+    public override void OnInteraction(InteractionManager interactor)
+    {
+
+        if (active)
+        {
+            base.OnInteraction(interactor);
+            if (!picked_up)
+            {
+                //transform.SetParent(item_holder.transform);
+                transform.position = item_holder.transform.position;
+                interactor.currentInteraction = this;
+                promptCanvas.enabled = false;
+            }
+            else {
+                interactor.currentInteraction = null;
+                promptCanvas.enabled = true;
+            }
+            picked_up = !picked_up;
+        }
+
+    }
+}
