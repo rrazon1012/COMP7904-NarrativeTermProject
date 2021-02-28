edba4be8bb467ac7ad90722d7118dcb9ea14fc5c
zzhang129@my.bcit.ca <zzhang129@my.bcit.ca>
2021-02-27 18:53:03 -0800

    Set up actions we will need to preform during game

--- Assets/_Modules/GameMechanics/EventSystem.cs
+++ Assets/_Modules/GameMechanics/EventSystem.cs
@@ -5,15 +5,52 @@ using UnityEngine;
 
 public class EventSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public static EventSystem current;
 
+    public void Awake()
+    {
+        current = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public event Action onPlayerRangeEnter;
+    public void PlayerRangeEnter()
     {
+        onPlayerRangeEnter?.Invoke();
+    }
 
+    public event Action onPlayerRangeExit;
+    public void PlayerRangeExit()
+    {
+        onPlayerRangeExit?.Invoke();
+    }
+
+    public event Action onObjectRangeEnter;
+    public void ObjectRangeEnter()
+    {
+        onPlayerRangeEnter?.Invoke();
+    }
+
+    public event Action onObjectRangeExit;
+    public void ObjectRangeExit()
+    {
+        onObjectRangeExit?.Invoke();
+    }
+
+    public event Action onObjectInteract;
+    public void ObjectInteract()
+    {
+        onObjectInteract?.Invoke();
+    }
+
+    public event Action onObjectFinish;
+    public void ObjectFinish()
+    {
+        onObjectFinish?.Invoke();
+    }
+
+    public event Action onPlayerDeath;
+    public void PlayerDeath()
+    {
+        onPlayerDeath?.Invoke();
     }
 }
