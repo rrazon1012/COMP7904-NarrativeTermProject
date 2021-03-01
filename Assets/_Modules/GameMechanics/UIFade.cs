3ee8c39751136e0103a1355ea1d6cca7062322d8
zzhang129@my.bcit.ca <zzhang129@my.bcit.ca>
2021-02-28 16:24:29 -0800

    UIFade implemented

--- Assets/_Modules/GameMechanics/UIFade.cs
+++ Assets/_Modules/GameMechanics/UIFade.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class UIFade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static IEnumerator FadeCanvas(CanvasGroup canvas, float startAlpha, float endAlpha, float duration, float waitTime)
     {
-        
-    }
+        yield return new WaitForSeconds(waitTime);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var startTime = Time.time;
+        var endTime = Time.time + duration;
+        var elapsedTime = 0f;
+
+        canvas.alpha = startAlpha;
+
+        while (Time.time <= endTime)
+        {
+            elapsedTime = Time.time - startTime;
+            var percentage = 1 / (duration / elapsedTime);
+            if (startAlpha > endAlpha)
+            {
+                canvas.alpha = startAlpha - percentage;
+            }
+            else if (startAlpha < endAlpha)
+            {
+                canvas.alpha = startAlpha + percentage;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        canvas.alpha = endAlpha;
     }
 }
