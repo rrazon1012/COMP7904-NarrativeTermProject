7b4af8f795724d3966a64887edb04c17b6c303f5
hgholami <35938542+hgholami@users.noreply.github.com>
2021-03-29 11:20:12 -0700

    Key added for Entrance

--- Assets/event_key.cs
+++ Assets/event_key.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class event_key : MonoBehaviour
 {
+    [SerializeField] protected intr_Door door;
+    [SerializeField] protected string keyName;
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Entering Item Trigger");
 
-        if (other.tag == "ItemTrigger") {
+        if (other.name.Equals(keyName)) {
 
-            Debug.Log("Firing event");
-
+            door.LockOpen();
             EventSystem.current.KeyEnterTrigger();
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            //Debug.Log("Firing event");
         }
     }
 }
