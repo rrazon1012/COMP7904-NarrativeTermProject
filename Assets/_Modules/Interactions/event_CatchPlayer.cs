cf14f5592e704d6ff874b16598f211fabcf488f5
rrazon1012 <rammel1012@gmail.com>
2021-03-28 22:36:35 -0700

    respawns player and enemy

---
+++ Assets/_Modules/Interactions/event_CatchPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class event_CatchPlayer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player" && GameManager.GM.enemy.GetComponent<VoidToggledEnemy>().IsRevealed)
+        {
+
+            Debug.Log("Firing event");
+
+            EventSystem.current.PlayerCaughtTrigger();
+        }
+    }
+}
