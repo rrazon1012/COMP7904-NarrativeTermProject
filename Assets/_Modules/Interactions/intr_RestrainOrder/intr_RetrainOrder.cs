1b9b8ceeb2b3c882b29057dd4c9c3492888f6152
zzhang129@my.bcit.ca <zzhang129@my.bcit.ca>
2021-04-16 20:43:02 -0700

    Attic lock and ladder added sfx

---
+++ Assets/_Modules/Interactions/intr_RestrainOrder/intr_RetrainOrder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intr_RetrainOrder : InteractableObject
+{
+    //temporary access to main camer, might delegate to statemanager for an inspecting state
+    [SerializeField]
+    public GameObject main_Cam;
+    protected bool colorSwapped = false;
+    protected bool checkedRO = false;
+
+    void FixedUpdate()
+    {
+        UpdateInteraction();
+    }
+
+    public override void OnInteraction(InteractionManager interactor)
+    {
+        if (active)
+        {
+            Debug.Log("Inspect");
+            base.OnInteraction(interactor);
+            interactor.currentInteraction = this;
+            if (!checkedRO)
+            {
+                EventSystem.current.RestrainOrderCheck();
+                checkedRO = true;
+            }
+        }
+    }
+}
