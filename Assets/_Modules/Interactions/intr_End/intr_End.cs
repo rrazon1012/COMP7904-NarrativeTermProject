98413071ca80826546ff50053199f9caa6028e11
zzhang129@my.bcit.ca <zzhang129@my.bcit.ca>
2021-04-16 23:00:40 -0700

    Sofa sfx added

---
+++ Assets/_Modules/Interactions/intr_End/intr_End.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intr_End : InteractableObject
+{
+    void FixedUpdate()
+    {
+        UpdateInteraction();
+    }
+
+    public override void OnInteraction(InteractionManager interactor)
+    {
+
+        if (active)
+        {
+            base.OnInteraction(interactor);
+            EventSystem.current.OnPlayerInteractEnd();
+        }
+
+    }
+}
